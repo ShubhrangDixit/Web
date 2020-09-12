8e8ef76b56939abc6a7e50f00b6d3e8c35e6fb80
ShubhrangDixit <shubhrangdixit@gmail.com>
2020-09-13 00:01:38 +0530

    My First_eclipse

---
+++ webex/src/main/java/com/lti/CircleService.java
@@ -0,0 +1,17 @@
+package com.lti;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+
+@Path("/Circle")
+public class CircleService {
+	@GET
+	@Path("/area/{radius}")
+	
+	public String getArea(@PathParam(value = "radius") double radius) {
+		double area = Math.PI*radius*radius;
+		return String.valueOf(area);
+	}
+}
